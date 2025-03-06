f5d02b0398767990e807995ff88a49d70a2c3137
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-03-06 12:33:40 +0530

    [properties] added statistics

--- jpa_internals_1/src/main/java/com/java/jpa/entity/Student.java
+++ jpa_internals_1/src/main/java/com/java/jpa/entity/Student.java
@@ -7,10 +7,10 @@ import java.util.Set;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
+import jakarta.persistence.Cacheable;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -21,8 +21,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -32,31 +33,31 @@ import lombok.ToString;
 @Builder
 public class Student extends BaseData implements Serializable {
 	private static final long serialVersionUID = 339912208386100188L;
-	@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
+	
 	@Column(name = "rollno", unique = true)
 	private String rollno;
-	@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
+	
 	@Column(name = "firstname")
 	private String firstname;
-	@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
+	
 	@Column(name = "lastname")
 	private String lastname;
-	@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
+	
 	@Column(name = "address")
 	private String address;
-	@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
+	
 	@Column(name = "mobileNumber")
 	private long mobileNumber;
-	@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
+	
 	@Column(name = "age")
 	private byte age;
-	@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
+	
 	@Column(name = "isStudent")
 	private boolean isStudent;
-	@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
+	
 	@Column(name = "courseRegisteredIn")
 	private int courseRegisteredIn;
-	@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
+	
 	@EqualsAndHashCode.Exclude
 	@Builder.Default
 	@ManyToMany(cascade = CascadeType.PERSIST)
