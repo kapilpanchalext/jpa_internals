15fede2460c6027754a0d900ce0e33e5a5523924
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-03-25 16:57:36 +0530

    [kafka] messages

--- jpa_internals_1/src/main/java/com/java/jpa/api/HelloWorldJPA.java
+++ jpa_internals_1/src/main/java/com/java/jpa/api/HelloWorldJPA.java
@@ -17,8 +17,10 @@ import lombok.RequiredArgsConstructor;
 public class HelloWorldJPA {
 	
 	private final KafkaTemplate<String, String> template;
+	
 
 	@GetMapping(path = "/helloworld")
+	
 	public ResponseEntity<String> getHelloWorld(){
 		return ResponseEntity
 					.status(HttpStatus.OK)
@@ -28,6 +30,21 @@ public class HelloWorldJPA {
 	@PostMapping(path = "/postMessage")
 	public ResponseEntity<String> sendKafkaMessage(@RequestBody String message){
 		template.send("helloworld-topic1", message);
-		return ResponseEntity.status(HttpStatus.OK).body("Message Sent Successfully!");
+		System.err.println(message);
+		return ResponseEntity
+					.status(HttpStatus.OK)
+					.body("Message Sent Successfully!");
+	}
+	
+	@PostMapping(path = "/message")
+	public ResponseEntity<String> sendKafkaMessageMany() throws InterruptedException {
+		for(int i = 0; i<Integer.MAX_VALUE; i++) {
+			template.send("helloworld-topic1", "This is message no: " + i + " of total messages.");
+			Thread.sleep(1000);
+		}
+		
+		return ResponseEntity
+					.status(HttpStatus.OK)
+					.body("Message Sent Successfully!");
 	}
 }
