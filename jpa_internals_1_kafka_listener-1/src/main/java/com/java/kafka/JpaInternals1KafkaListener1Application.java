82f00606901787afc004e9281962a70535df652a
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-03-25 13:54:59 +0530

    [template]

---
+++ jpa_internals_1_kafka_listener-1/src/main/java/com/java/kafka/JpaInternals1KafkaListener1Application.java
@@ -0,0 +1,13 @@
+package com.java.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaInternals1KafkaListener1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaInternals1KafkaListener1Application.class, args);
+	}
+
+}
---
+++ jpa_internals_1_kafka_listener-1/src/test/java/com/java/kafka/JpaInternals1KafkaListener1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaInternals1KafkaListener1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
