1174ade00b5b2aa78738fe6435a0103b995de4fc
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-04-01 09:28:53 +0530

    [kafka] producer

---
+++ apache_kafka_producer_1/src/main/java/com/java/producer/ApacheKafkaProducer1Application.java
@@ -0,0 +1,13 @@
+package com.java.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheKafkaProducer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheKafkaProducer1Application.class, args);
+	}
+
+}
