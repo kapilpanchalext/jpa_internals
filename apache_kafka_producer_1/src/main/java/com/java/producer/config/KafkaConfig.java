35ed15ff5562c5a3564e85e1c6df93ee63a528d8
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-04-01 15:08:41 +0530

    [properties] kafka

--- apache_kafka_producer_1/src/main/java/com/java/producer/config/KafkaConfig.java
+++ apache_kafka_producer_1/src/main/java/com/java/producer/config/KafkaConfig.java
@@ -1,14 +1,67 @@
 package com.java.producer.config;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import com.java.producer.event.ProductCreatedEvent;
 
 @Configuration
 public class KafkaConfig {
+	
+//	@Value("${spring.kafka.bootstrap-servers}")
+//	private String bootstrapServers;
+//	
+//	@Value("${spring.kafka.producer.acks}")
+//	private String acks;
+//	
+//	@Value("${spring.kafka.producer.key-serializer}")
+//	private String keySerializer;
+//	
+//	@Value("${spring.kafka.producer.value-serializer}")
+//	private String valueSerializer;
+//	
+//	@Value("${spring.kafka.producer.properties.delivery.timeout.ms}")
+//	private String deliveryTimeouts;
+//	
+//	@Value("${spring.kafka.producer.properties.linger.ms}")
+//	private String lingerMsConfig;
+//	
+//	@Value("${spring.kafka.producer.properties.request.timeout.ms}")
+//	private String producerRequestTimeout;
+//	
+//	Map<String, Object> producerConfigs() {
+//		Map<String, Object> config = new HashMap<>();
+//		
+//		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+//		config.put(ProducerConfig.ACKS_CONFIG, acks);
+//		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+//		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, config);
+//		config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeouts);
+//		config.put(ProducerConfig.LINGER_MS_CONFIG, lingerMsConfig);
+//		config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, producerRequestTimeout);
+//		
+//		return config;
+//	}
+//	
+//	@Bean
+//	ProducerFactory<String, ProductCreatedEvent> producerFactory() {
+//		return new DefaultKafkaProducerFactory<>(producerConfigs());
+//	}
+//	
+//	@Bean
+//	KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate(){
+//		return new KafkaTemplate<>(producerFactory());
+//	}
 
 	@Bean
 	NewTopic createTopic() {
--- apache_kafka_producer_1/src/main/java/com/java/producer/service/ProductServiceImpl.java
+++ apache_kafka_producer_1/src/main/java/com/java/producer/service/ProductServiceImpl.java
@@ -18,18 +18,18 @@ import lombok.RequiredArgsConstructor;
 public class ProductServiceImpl implements ProductService {
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	private final KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
-	
+
 	@Override
 	public String createProduct(CreateProductRestModel productRestModel) throws Exception {
 		String productId = UUID.randomUUID().toString();
-		
+
 		// TODO: Persist Product Details into Database before publishing an Event
 		ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(
 				productId, 
 				productRestModel.getTitle(), 
 				productRestModel.getPrice(), 
 				productRestModel.getQuantity());
-		
+
 		// Asynchronous Message Send
 //		CompletableFuture<SendResult<String, ProductCreatedEvent>> future = 
 //				kafkaTemplate.send("product-created-events-topic", productId, productCreatedEvent);
@@ -41,17 +41,26 @@ public class ProductServiceImpl implements ProductService {
 //				LOGGER.info("*****Message was sent Successfully: " + result.getRecordMetadata());
 //			}
 //		});
-		
+
 		// Synchronous Message Send
+//		LOGGER.info("*****Before publishing product created event: " + productId);
+//		SendResult<String, ProductCreatedEvent> result = 
+//				kafkaTemplate.send("product-created-events-topic", productId, productCreatedEvent).get();
+//		
+//		LOGGER.info("Partition: " + result.getRecordMetadata().partition());
+//		LOGGER.info("Topic: " + result.getRecordMetadata().topic());
+//		LOGGER.info("Offset: " + result.getRecordMetadata().offset());
+//		LOGGER.info("*****Returning product id: " + productId);
+
 		LOGGER.info("*****Before publishing product created event: " + productId);
 		SendResult<String, ProductCreatedEvent> result = 
-				kafkaTemplate.send("product-created-events-topic", productId, productCreatedEvent).get();
-		
+				kafkaTemplate.send("topic2", productId, productCreatedEvent).get();
+
 		LOGGER.info("Partition: " + result.getRecordMetadata().partition());
 		LOGGER.info("Topic: " + result.getRecordMetadata().topic());
 		LOGGER.info("Offset: " + result.getRecordMetadata().offset());
-		
 		LOGGER.info("*****Returning product id: " + productId);
+
 		return productId;
 	}
 }
