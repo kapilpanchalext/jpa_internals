c5b40c99d642f7bde812247263f7b2cae991e067
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-04-01 15:45:27 +0530

    [producer]

--- apache_kafka_producer_1/src/main/java/com/java/producer/config/KafkaConfig.java
+++ apache_kafka_producer_1/src/main/java/com/java/producer/config/KafkaConfig.java
@@ -1,19 +1,11 @@
 package com.java.producer.config;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-
-import com.java.producer.event.ProductCreatedEvent;
 
 @Configuration
 public class KafkaConfig {
