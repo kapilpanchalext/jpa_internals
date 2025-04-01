794631fe251512d10d2214c56d0adbcce716d0be
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-04-01 15:46:16 +0530

    [consumer] init

---
+++ email-notification-1/src/main/java/com/java/email/EmailNotification1Application.java
@@ -0,0 +1,13 @@
+package com.java.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailNotification1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailNotification1Application.class, args);
+	}
+
+}
