7a7f20850f37a2fdc79323a4757a389c6079096a
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-04-02 12:47:49 +0530

    [mock] microservice

---
+++ mock-service-1/src/main/java/com/java/mock/MockService1Application.java
@@ -0,0 +1,13 @@
+package com.java.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockService1Application.class, args);
+	}
+
+}
---
+++ mock-service-1/src/test/java/com/java/mock/MockService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
