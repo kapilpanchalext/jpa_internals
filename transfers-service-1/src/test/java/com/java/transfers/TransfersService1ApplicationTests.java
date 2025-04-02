d78ac8ca37b34f4092b3bdde004d562fc4222dd4
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-04-02 16:51:11 +0530

    [init]

---
+++ transfers-service-1/src/test/java/com/java/transfers/TransfersService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.transfers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfersService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
