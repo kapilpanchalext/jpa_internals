741ec0f66feb9425d6845a08d01bc82fddb7abc4
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-04-03 10:23:36 +0530

    [init] deposit service

---
+++ deposit-service-1/src/main/java/com/java/deposit/exception/RetryableException.java
@@ -0,0 +1,15 @@
+package com.java.deposit.exception;
+
+public class RetryableException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public RetryableException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RetryableException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+}
