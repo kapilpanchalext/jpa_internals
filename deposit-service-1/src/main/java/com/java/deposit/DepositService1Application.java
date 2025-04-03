4dc0271dc248c9d90d68837fce27face2cbd27ac
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-04-03 10:23:56 +0530

    [init]

---
+++ deposit-service-1/src/main/java/com/java/deposit/DepositService1Application.java
@@ -0,0 +1,13 @@
+package com.java.deposit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositService1Application.class, args);
+	}
+
+}
---
+++ deposit-service-1/src/test/java/com/java/deposit/DepositService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.deposit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
