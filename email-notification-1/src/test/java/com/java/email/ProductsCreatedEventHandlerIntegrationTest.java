07475df67f06feed07314eb11013d87f4f72eeab
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-04-04 12:59:39 +0530

    [refactor]

--- email-notification-1/src/test/java/com/java/email/ProductsCreatedEventHandlerIntegrationTest.java
+++ email-notification-1/src/test/java/com/java/email/ProductsCreatedEventHandlerIntegrationTest.java
@@ -6,7 +6,6 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.timeout;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
