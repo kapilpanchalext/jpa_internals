98daf273875cd218de3f5ef9826e4a0f0abd0659
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-04-15 15:22:32 +0530

    [config]
    Code Quality:
    - formatted -   [ ]
    - mvn clean -   [ ]
    - mvn install - [ ]
    
    Related ticket(s):

--- products-service-1/src/main/java/com/java/products/config/KafkaConfig.java
+++ products-service-1/src/main/java/com/java/products/config/KafkaConfig.java
@@ -1,84 +1,30 @@
 package com.java.products.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-
-import com.java.products.event.ProductCreatedEvent;
 
 @Configuration
 public class KafkaConfig {
 
-	@Value("${spring.kafka.bootstrap-servers}")
-	private String bootstrapServers;
+	@Value("${products.events.topic.name}")
+	private String productsEventsTopicName;
 
-    @Value("${spring.kafka.producer.key-serializer}")
-    private String keySerializer;
+	private final static int TOPIC_REPLICATION_FACTOR = 3;
+	private final static int TOPIC_PARTITIONS = 3;
 
-    @Value("${spring.kafka.producer.value-serializer}")
-    private String valueSerializer;
-
-    @Value("${spring.kafka.producer.acks}")
-    private String acks;
-
-    @Value("${spring.kafka.producer.properties.delivery.timeout.ms}")
-    private String deliveryTimeout;
-
-    @Value("${spring.kafka.producer.properties.linger.ms}")
-    private String linger;
-
-    @Value("${spring.kafka.producer.properties.request.timeout.ms}")
-    private String requestTimeout;
-
-    @Value("${spring.kafka.producer.properties.enable.idempotence}")
-    private boolean idempotence;
-
-    @Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connection}")
-    private Integer inflightRequests;
-
-	Map<String, Object> producerConfigs() {
-		Map<String, Object> config = new HashMap<>();
-
-		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
-		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
-		config.put(ProducerConfig.ACKS_CONFIG, acks);
-		config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
-		config.put(ProducerConfig.LINGER_MS_CONFIG, linger);
-		config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
-		config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, idempotence);
-		config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, inflightRequests);
-		config.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "transfer-service");
-		//config.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
-
-		return config;
+	@Bean
+	KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+		return new KafkaTemplate<>(producerFactory);
 	}
 
 	@Bean
-	ProducerFactory<String, ProductCreatedEvent> producerFactory() {
-		return new DefaultKafkaProducerFactory<>(producerConfigs());
-	}
-
-	@Bean
-	KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate() {
-		return new KafkaTemplate<String, ProductCreatedEvent>(producerFactory());
-	}
-
-	@Bean
-	NewTopic createTopic() {
-		return TopicBuilder.name("product-created-events-topic")
-				.partitions(3)
-				.replicas(3)
-				.configs(Map.of("min.insync.replicas","2"))
-				.build();
+	NewTopic createProductsEventsTopic() {
+		return TopicBuilder.name(productsEventsTopicName).partitions(TOPIC_PARTITIONS)
+				.replicas(TOPIC_REPLICATION_FACTOR).build();
 	}
 }
